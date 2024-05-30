ca4c9568c7b6ae7a7cc4b66e7e591ce7303f5488
Brian <150264424+As132030720@users.noreply.github.com>
2024-05-31 01:38:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,9 @@ def handle_message(event):
     elif '手機' in msg:
         message = test()
         line_bot_api.reply_message(event.reply_token, message)
+    elif '餐廳' in msg:
+        message = restaurant()
+        line_bot_api.reply_message(event.reply_token, message)
     elif '列表' in msg:
         message = function_list()
         line_bot_api.reply_message(event.reply_token, message)
